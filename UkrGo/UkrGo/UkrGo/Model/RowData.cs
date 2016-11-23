9242cf27bb4f9272b77d3b90aee40b77ed437f18
POLUBIN\Andrey <polubin@yandex.ru>
2016-11-23 22:54:17 +0200

    First commit

---
+++ UkrGo/UkrGo/UkrGo/Model/RowData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UkrGo.Model
+{
+    public class RowData
+    {
+        public string ImageLink { get; set; }
+        public string Link { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
